a03072a79da28f78877be5d9f25bb3edce6b16e8
Marco Cavalli <cmarcos46@gmail.com>
2020-05-30 18:14:48 +0200

    Update build.gradle, AndroidManifest.xml, and 21 more files...

--- lost_and_found/app/src/main/java/com/marco_cavalli/lost_and_found/AuthenticationScreen.java
+++ lost_and_found/app/src/main/java/com/marco_cavalli/lost_and_found/AuthenticationScreen.java
@@ -33,7 +33,6 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
 
-import java.util.Arrays;
 import java.util.Collections;
 
 public class AuthenticationScreen extends BaseActivity {
